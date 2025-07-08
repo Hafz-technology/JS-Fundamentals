68678ada3bba8a11f52c108bd4db30239376ba50
Khalil Hafz <khalilhafz@gmail.com>
2025-07-08 22:02:27 +0300

    create 8-square.js

---
+++ 8-square.js
@@ -0,0 +1,11 @@
+const arguments = process.argv;
+let firstArgument = arguments[2];
+
+firstArgument = parseInt(firstArgument);
+if (isNaN(firstArgument)) {
+    console.log('Missing size');
+} else {
+    for (let i = 0; i < firstArgument; i++) {
+        console.log('x'.repeat(firstArgument));
+    }
+}
