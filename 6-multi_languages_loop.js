67359232977ea3772eb10612f80e96697c560da6
Khalil Hafz <khalilhafz@gmail.com>
2025-07-08 21:30:03 +0300

    update

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 
 let outPut = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for(let i = 0; i < outPut.length; i++ ) {
+for (let i = 0; i < outPut.length; i++ ) {
     console.log(outPut[i]);
 }
 
