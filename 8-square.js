abcd40b5d7e74cdcf4ee17016b064ecad8f8f0f6
Khalil Hafz <khalilhafz@gmail.com>
2025-07-08 22:32:39 +0300

    create 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -6,6 +6,13 @@ if (isNaN(firstArgument)) {
     console.log('Missing size');
 } else {
     for (let i = 0; i < firstArgument; i++) {
-        console.log('x'.repeat(firstArgument));
+        console.log('X'.repeat(firstArgument));
     }
 }
+
+
+// Example using process.stdout.write()
+process.stdout.write("This will be on the same line.");
+process.stdout.write("This continues on the same line.\n"); // Add \n manually for a newline
+
+console.log("This will start on a new line and add its own newline.");
