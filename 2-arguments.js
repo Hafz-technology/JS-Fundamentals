37ffeaff3d3ba49a71f9f91b1f87271852abd7b4
Khalil Hafz <khalilhafz@gmail.com>
2025-07-08 20:30:19 +0300

    create 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+let numberOfArguments = 2 ;
+if (numberOfArguments === 0) {
+    console.log('No argument');
+}
+else if (numberOfArguments === 1) {
+    console.log('Argument found');  
+} else if (numberOfArguments > 1) {
+    console.log('Arguments found')
+}
+
