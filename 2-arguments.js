c4607bdb1b55b9535aab448b286bb1b44def557c
Khalil Hafz <khalilhafz@gmail.com>
2025-07-08 20:38:41 +0300

    Update

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
 let numberOfArguments = ["khalil", 'ahmed'] ;
-if (numberOfArguments.length === 0) {
+if (numberOfArguments.length == 0) {
     console.log('No argument');
 }
-else if (numberOfArguments.length === 1) {
+else if (numberOfArguments.length == 1) {
     console.log('Argument found');  
 } else if (numberOfArguments.length > 1) {
     console.log('Arguments found')
